4697752fec24097cc3e8fab0bbda0e04a26fc6cc
Caged <justin@labratrevenge.com>
2010-02-08 10:56:42 -0800

    fix copy

--- Source/HTTPRiot/Utilities/HRBase64.h
+++ Source/HTTPRiot/Utilities/HRBase64.h
@@ -5,7 +5,7 @@
 //  Created by Justin Palmer on 7/2/09.
 //  Copyright 2009 LabratRevenge LLC.. All rights reserved.
 //
-//  This was taken Cyrus' Public Domain implementation on the bottom of 
+//  This was taken from Cyrus' Public Domain implementation on the bottom of 
 //  http://www.cocoadev.com/index.pl?BaseSixtyFour.
 //
 #import <Foundation/Foundation.h>
